605dc0889d69f410fbcac4757836e68473ac42ea
Megan Schuetz <mschuetz411@gmail.com>
2022-04-19 17:31:03 -0600

    Start html and player and game class

---
+++ player.js
@@ -0,0 +1,14 @@
+class Player {
+  constructor(id, token, wins){
+    this.id = id;
+    this.token = token;
+    this.wins = wins;
+  }
+
+  increaseWins(){
+    this.wins++;
+  }
+}
+
+
+module.exports = Player;
