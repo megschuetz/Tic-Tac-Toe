6fac4a3f90525780124181b2bbab23db9fc35d0a
Megan Schuetz <mschuetz411@gmail.com>
2022-04-21 11:49:42 -0600

    Allow players to take turns and check to see if the player is winning

--- game.js
+++ game.js
@@ -7,25 +7,66 @@ class Game {
       '3', '4', '5',
       '6', '7', '8'
     ]
-    this.numOfTurnsPlayed = 0;
+    this.countTurns = 0;
 
+    //should maybe be global variables - why didnt this work -
+    // this.winningKeys = [
+    //   this.board[0,1,2].includes(this.currentPlayer.token),
+    //   this.board[3,4,5].includes(this.currentPlayer.token),
+    //   this.board[6,7,8].includes(this.currentPlayer.token),
+    //   this.board[0,3,6].includes(this.currentPlayer.token),
+    //   this.board[1,4,7].includes(this.currentPlayer.token),
+    //   this.board[2,5,8].includes(this.currentPlayer.token),
+    //   this.board[0,4,8].includes(this.currentPlayer.token),
+    //   this.board[2,4,6].includes(this.currentPlayer.token),
+    // ]
   }
 
-  chooseSpace(choosen){
+
+
+  chooseSpace(choosen) {
     for (var i = 0; i < this.board.length; i++) {
       if (this.board[i] == choosen){
         this.board.splice(i, 1, this.currentPlayer.token)
       }
     }
+
+    this.countTurns++;
+    if (this.countTurns >= 5){
+      this.checkForWinning();
+    }
   }
 
-  recordBoxClicked(){
 
+
+
+  checkForWinning() {
+    var winningKeys = [
+      this.board[0,1,2].includes(this.currentPlayer.token),
+      this.board[3,4,5].includes(this.currentPlayer.token),
+      this.board[6,7,8].includes(this.currentPlayer.token),
+      this.board[0,3,6].includes(this.currentPlayer.token),
+      this.board[1,4,7].includes(this.currentPlayer.token),
+      this.board[2,5,8].includes(this.currentPlayer.token),
+      this.board[0,4,8].includes(this.currentPlayer.token),
+      this.board[2,4,6].includes(this.currentPlayer.token),
+    ]
+
+
+    for (var i = 0; i < winningKeys.length; i++) {
+      if (winningKeys[i]) {
+        console.log('Winner winner chicken dinner!!')
+      }
+    }
   }
 
   switchTurns(){
-    console.log(this.players[1]);
-    this.currentPlayer = this.players[1];
+
+    if (this.currentPlayer === this.players[0]) {
+      this.currentPlayer = this.players[1]
+    } else {
+      this.currentPlayer = this.players[0]
+    }
   }
 }
 
