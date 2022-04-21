f7a4838f333fa10399a4e1aaff6bf11974a9c4b3
Megan Schuetz <mschuetz411@gmail.com>
2022-04-20 18:26:29 -0600

    Create Player class and startthe board choosing a space and taking turns in Game class.

--- game.js
+++ game.js
@@ -1,8 +1,33 @@
 class Game {
-  constructor(){
+  constructor(player1, player2){
+    this.players = [player1, player2];
+    this.currentPlayer = player1;
+    this.board = [
+      '0', '1', '2',
+      '3', '4', '5',
+      '6', '7', '8'
+    ]
+    this.numOfTurnsPlayed = 0;
 
   }
-   
+
+  chooseSpace(choosen){
+    for (var i = 0; i < this.board.length; i++) {
+      if (this.board[i] == choosen){
+        this.board.splice(i, 1, this.currentPlayer.token)
+      }
+    }
+  }
+
+  recordBoxClicked(){
+
+  }
+
+  switchTurns(){
+    console.log(this.players[1]);
+    this.currentPlayer = this.players[1];
+  }
 }
 
-module.exports = Game;
+
+//after one player has 3 turns check if winning ==> have a counter in the constructor
---
+++ main.js
@@ -0,0 +1,2 @@
+var coconut = new Player(1,'OH', 0)
+var pineapple = new Player(2, 'X', 0)
