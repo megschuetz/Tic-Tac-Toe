4e792e0051478f3ad3fcb0320dbf595b8475e10b
Megan Schuetz <mschuetz411@gmail.com>
2022-04-21 16:15:34 -0600

    add background photo and more css

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
-var coconut = new Player(1,'OH', 0)
-var pineapple = new Player(2, 'X', 0)
+var pineapple = new Player(1, '🥥', 0)
+var coconut = new Player(2,'🐚', 0)
+var startGame = new Game(pineapple, coconut)
+
+var header = document.querySelector('h1')
+var clickSpot = document.querySelector('.board')
+
+
+
+
+
+window.addEventListener('load', startGame)
+clickSpot.addEventListener('click', showToken)
+
+
+
+function startGame(){
+  header.innerText = `It's ${startGame.currentPlayer.token}'s turn`
+}
+
+function showToken(event){
+  startGame.chooseSpace(event.target.id)
+  event.target.innerText = startGame.currentPlayer.token
+}
